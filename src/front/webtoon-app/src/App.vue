b1941b12d1282bbbe1575ff74e0865d472bd6466
ToasT1ng <jrainboww9@gmail.com>
2025-02-25 23:14:19 +0900

    Vue.js 3 + Vuetify 로 간단 프론트엔드 세팅

---
+++ src/front/webtoon-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-btn color="primary">Vuetify Button</v-btn>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
--- src/main/kotlin/com/toast1ng/webtoon/product/adapter/out/persistence/WebtoonProductJpaEntity.kt
+++ src/main/kotlin/com/toast1ng/webtoon/product/adapter/out/persistence/WebtoonProductJpaEntity.kt
@@ -1,11 +1,15 @@
 package com.toast1ng.webtoon.product.adapter.out.persistence
 
 import com.toast1ng.webtoon.product.domain.WebtoonStatus
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import java.math.BigDecimal
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 
-data class WebtoonProductJpaEntity(
+@Entity
+class WebtoonProductJpaEntity(
+    @Id
     val id: String,
     val title: String,
     val authorId: String,
