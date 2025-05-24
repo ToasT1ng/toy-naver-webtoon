84dc61a389bdbd0366502d35f1735460b32c670b
ToasT1ng <jrainboww9@gmail.com>
2025-05-24 21:56:36 +0900

    ✨ Refactor WebtoonDetail component; extract episode list into WebtoonEpisodeList component for improved structure and maintainability

--- src/front/webtoon-app/src/components/webtoon/WebtoonDetail.vue
+++ src/front/webtoon-app/src/components/webtoon/WebtoonDetail.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-
-import {computed, onMounted, ref, watch} from "vue";
-import {navigateToWebtoonEpisode} from "@/utils/navigation";
-import {useWebtoonEpisodes} from "@/composables/useWebtoonEpisodes";
+import {onMounted, watch} from "vue";
 import {useWebtoon} from "@/composables/useWebtoons";
+import WebtoonEpisodeList from "@/components/webtoon/WebtoonEpisodeList.vue";
 
 interface Props {
   webtoonId: string
@@ -14,11 +12,6 @@ const props = defineProps<Props>()
 
 //TODO: webtoonId를 store에서 관리
 const {data: webtoonData, updateWebtoonId: updateWebtoonId} = useWebtoon()
-const {
-  data: webtoonEpisodesData,
-  updateWebtoonId: updateWebtoonEpisodesWebtoonId,
-  updatePageNo: updateWebtoonEpisodesPageNo
-} = useWebtoonEpisodes()
 
 const daysOfWeek = [
   {name: '월요', value: 'mon'},
@@ -35,54 +28,16 @@ function makDayOfWeekString(inputDay: string) {
   return dayOfWeek ? dayOfWeek.name : ''
 }
 
-const totalCount = computed(() => webtoonEpisodesData.value?.totalCount ?? 0)
-const episodes = computed(() => webtoonEpisodesData.value?.content ?? [])
-
-function formatDate(dateString: string) {
-  const date = new Date(dateString)
-  const yy = String(date.getFullYear()).slice(2)
-  const mm = String(date.getMonth() + 1).padStart(2, '0')
-  const dd = String(date.getDate()).padStart(2, '0')
-  return `${yy}.${mm}.${dd}`
-}
-
-type SortOption = 'asc' | 'dec'
-
-const selectedSort = ref<SortOption>('asc')
-
-const sortOptions = [
-  {label: '최신화부터', value: 'dec'},
-  {label: '1화부터', value: 'asc'},
-] satisfies readonly { label: string; value: SortOption }[]
-
-function onSort() {
-  console.log(selectedSort.value) //TODO api 재호출
-}
-
-function onClickEpisode(episodeId: number) {
-  navigateToWebtoonEpisode(Number(props.webtoonId), episodeId, props.tab)
-}
-
-const page = ref(1)
-const size = 10
-
 watch(
     () => props.webtoonId,
     (newWebtoonId) => {
       if (newWebtoonId) {
-        updateWebtoonEpisodesWebtoonId(Number(newWebtoonId))
         updateWebtoonId(Number(newWebtoonId))
       }
     },
     {immediate: true}
 )
 
-watch(() => page.value,
-    (newPage) => {
-      console.log("Updating page number to:", newPage)
-      updateWebtoonEpisodesPageNo(newPage - 1)
-    }
-)
 
 onMounted(() => {
   console.log('webtoonId:', props.webtoonId)
@@ -140,67 +95,7 @@ onMounted(() => {
     <!-- 광고 배너  -->
   </v-card>
   <v-card class="pa-4" elevation="0">
-    <div class="d-flex justify-space-between align-center">
-      <div class="entire-episode text-body-1 font-weight-bold text-grey-darken-1 pa-0 ma-0">
-        총 {{ totalCount }}화
-      </div>
-
-      <v-btn-toggle
-          v-model="selectedSort"
-          color="black"
-          variant="plain"
-          class="pa-0 ma-0"
-          style="height: 24px; line-height: 24px; min-height: 24px;"
-          mandatory
-      >
-        <v-btn
-            v-for="option in sortOptions"
-            :key="option.value"
-            :value="option.value"
-            @click="onSort"
-            small
-            elevation="0"
-            :ripple="false"
-            class="pa-0"
-        >
-          {{ option.label }}
-        </v-btn>
-      </v-btn-toggle>
-    </div>
-    <v-list>
-      <v-list-item
-          v-for="episode in episodes"
-          :key="episode.episodeId"
-          class="pa-0 pt-1 pb-1"
-          @click="onClickEpisode(episode.episodeId)"
-      >
-        <v-row no-gutters align="center">
-          <div style="width: 10%;" class="pt-1 pb-1 mr-4">
-            <v-img
-                class="with-border"
-                width="100%"
-                cover
-                :src="episode.thumbnail"
-            />
-          </div>
-          <div style="flex: 1;">
-            <div class="font-weight-bold text-body-1">{{ episode.title }}</div>
-            <div class="font-weight-bold text-grey text-body-2">
-              ★ {{ episode.rating }} &nbsp; {{ formatDate(episode.uploadDate) }}
-            </div>
-          </div>
-        </v-row>
-      </v-list-item>
-    </v-list>
-
-    <div v-if="episodes.length > 0" class="d-flex justify-center mt-4">
-      <v-pagination
-          v-model="page"
-          :length="Math.ceil(totalCount / size)"
-          total-visible="5"
-          color="black"
-      />
-    </div>
+    <WebtoonEpisodeList :webtoon-id="props.webtoonId" :tab="props.tab"/>
   </v-card>
 </template>
 
@@ -215,31 +110,4 @@ onMounted(() => {
   color: black;
 }
 
-.with-border {
-  border: 1px #ccc;
-  border-radius: 3px;
-}
-
-.v-list-item {
-  border-bottom: 1px solid #e0e0e0;
-}
-
-.v-list-item:first-child {
-  border-top: 1px solid #e0e0e0;
-}
-
-::v-deep(.v-btn + .v-btn::before) {
-  content: "";
-  width: 1px;
-  height: 12px;
-  margin-left: 10px;
-  margin-right: 10px;
-  background-color: #e0e0e0;
-  display: inline-block;
-  vertical-align: middle;
-}
-
-::v-deep(.v-list-item:hover .text-body-1) {
-  text-decoration: underline;
-}
 </style>
---
+++ src/front/webtoon-app/src/components/webtoon/WebtoonEpisodeList.vue
@@ -0,0 +1,151 @@
+<script setup lang="ts">
+import {computed, ref, watch} from "vue";
+import {navigateToWebtoonEpisode} from "@/utils/navigation";
+import {useWebtoonEpisodes} from "@/composables/useWebtoonEpisodes";
+
+interface Props {
+  webtoonId: string
+  tab: string
+}
+const props = defineProps<Props>()
+
+const {
+  data: webtoonEpisodesData,
+  updateWebtoonId: updateWebtoonEpisodesWebtoonId,
+  updatePageNo: updateWebtoonEpisodesPageNo
+} = useWebtoonEpisodes()
+
+const totalCount = computed(() => webtoonEpisodesData.value?.totalCount ?? 0)
+const episodes = computed(() => webtoonEpisodesData.value?.content ?? [])
+
+const page = ref(1)
+const size = 10
+
+type SortOption = 'asc' | 'dec'
+
+const selectedSort = ref<SortOption>('asc')
+
+const sortOptions = [
+  {label: '최신화부터', value: 'dec'},
+  {label: '1화부터', value: 'asc'},
+] satisfies readonly { label: string; value: SortOption }[]
+
+function formatDate(dateString: string) {
+  const date = new Date(dateString)
+  const yy = String(date.getFullYear()).slice(2)
+  const mm = String(date.getMonth() + 1).padStart(2, '0')
+  const dd = String(date.getDate()).padStart(2, '0')
+  return `${yy}.${mm}.${dd}`
+}
+
+function onSort() {
+  console.log(selectedSort.value) //TODO api 재호출
+}
+
+function onClickEpisode(episodeId: number) {
+  navigateToWebtoonEpisode(Number(props.webtoonId), episodeId, props.tab)
+}
+
+watch(
+    () => props.webtoonId,
+    (newWebtoonId) => {
+      if (newWebtoonId) {
+        updateWebtoonEpisodesWebtoonId(Number(newWebtoonId))
+      }
+    },
+    {immediate: true}
+)
+
+watch(() => page.value,
+    (newPage) => {
+      console.log("Updating page number to:", newPage)
+      updateWebtoonEpisodesPageNo(newPage - 1)
+    }
+)
+</script>
+<template>
+  <div class="d-flex justify-space-between align-center">
+    <div class="entire-episode text-body-1 font-weight-bold text-grey-darken-1 pa-0 ma-0">
+      총 {{ totalCount }}화
+    </div>
+
+    <v-btn-toggle
+        v-model="selectedSort"
+        color="black"
+        variant="plain"
+        class="pa-0 ma-0"
+        style="height: 24px; line-height: 24px; min-height: 24px;"
+        mandatory
+    >
+      <v-btn
+          v-for="option in sortOptions"
+          :key="option.value"
+          :value="option.value"
+          @click="onSort"
+          small
+          elevation="0"
+          :ripple="false"
+          class="pa-0"
+      >
+        {{ option.label }}
+      </v-btn>
+    </v-btn-toggle>
+  </div>
+  <v-list>
+    <v-list-item
+        v-for="episode in episodes"
+        :key="episode.episodeId"
+        class="pa-0 pt-1 pb-1"
+        @click="onClickEpisode(episode.episodeId)"
+    >
+      <v-row no-gutters align="center">
+        <div style="width: 10%;" class="pt-1 pb-1 mr-4">
+          <v-img
+              class="with-border"
+              width="100%"
+              cover
+              :src="episode.thumbnail"
+          />
+        </div>
+        <div style="flex: 1;">
+          <div class="font-weight-bold text-body-1">{{ episode.title }}</div>
+          <div class="font-weight-bold text-grey text-body-2">
+            ★ {{ episode.rating }} &nbsp; {{ formatDate(episode.uploadDate) }}
+          </div>
+        </div>
+      </v-row>
+    </v-list-item>
+  </v-list>
+  <div v-if="episodes.length > 0" class="d-flex justify-center mt-4">
+    <v-pagination
+        v-model="page"
+        :length="Math.ceil(totalCount / size)"
+        total-visible="5"
+        color="black"
+    />
+  </div>
+</template>
+<style scoped>
+.v-list-item {
+  border-bottom: 1px solid #e0e0e0;
+}
+
+.v-list-item:first-child {
+  border-top: 1px solid #e0e0e0;
+}
+
+::v-deep(.v-btn + .v-btn::before) {
+  content: "";
+  width: 1px;
+  height: 12px;
+  margin-left: 10px;
+  margin-right: 10px;
+  background-color: #e0e0e0;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+::v-deep(.v-list-item:hover .text-body-1) {
+  text-decoration: underline;
+}
+</style>
